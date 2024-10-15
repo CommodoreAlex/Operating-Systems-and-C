cc9237a4646e7a2ee7f34cd866b655847c3e8054
CyberWired <CommodoreAlex@users.noreply.github.com>
2024-10-15 16:49:14 -0400

    Update overflow1.c

--- overflow1.c
+++ overflow1.c
@@ -1,16 +1,10 @@
-// sudo apt-get install gcc-multilib -y
-// gcc -o overflow1 -fno-stack-protector -m32 -z execstack overflow1.c
-// -fno-stack-protector
-// Remove the canary value at the end of the buffer: -fno-stack-protector
-// Set the program to compile into a 32 bit program: -m32
-// Make the stack executable: -z execstack
-
+// compile: gcc -fno-stack-protector -z execstack -no-pie overflow1.c -o overflow1
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
     char buf[256];
-    strcpy(buf, argv[1]);
-    printf("%s\n", buf);
+    strcpy(buf,argv[1]);
     return 0;
 }
