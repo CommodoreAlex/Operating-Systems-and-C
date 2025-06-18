4a13958553ec7289ab10a3428b446cad5c9aa40c
Alexander Commodore <CommodoreAlex@users.noreply.github.com>
2025-06-18 11:22:13 -0400

    Create counterWithoutLocks.c

---
+++ Practice/OperatingSystems/Scheduling/counterWithoutLocks.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// Example of a counter without locks
+
+// Creating a structure for a counter
+typedef struct __counter_t {
+    int value;
+} counter_t;
+
+// Initialize the counter
+void init (counter_t *c) {
+    c->value = 0;
+}
+
+// Increment the counter
+void increment (counter_t *c) {
+    c->value++;
+}
+
+// Decrement the counter
+void decrement (counter_t *c) {
+    c->value--;
+}
+
+// Get the value of counter
+int get (counter_t *c) {
+    return c->value;
+}
